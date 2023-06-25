dda8a3daf6f327187cde1f25ca5c707b9f749a1c
khayrullo <khayrullo.me@gmail.com>
2023-06-25 12:01:36 +0500

    add firebase crashList

--- app/src/main/java/uz/captan/food_up/MainActivity.kt
+++ app/src/main/java/uz/captan/food_up/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import uz.captan.food_up.ui.theme.Food_upTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
 
-                    
+
 
                 }
             }
